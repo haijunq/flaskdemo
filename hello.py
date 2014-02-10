2037ae84a94bf7061b0a7a21ddd0e0195443f01e
haijunq <haijunq@gmail.com>
2014-02-10 02:39:36 -0800

    add a userlist router

--- hello.py
+++ hello.py
@@ -1,8 +1,18 @@
 import os
+import json
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello World!'
+    return 'Hello hello World!'
+
+@app.route('/userlist')
+def userlist():
+    print request
+    response = {
+                "id" : 0, 
+                "name" : "haijun"
+                }
+    return json.dumps(response)
